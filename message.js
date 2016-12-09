bb2091be707417e94301726d0744829694eb0798
markgicharu <markgicharu@hotmail.com>
2016-12-09 15:44:44 +0300

    Add files via upload

---
+++ message.js
@@ -0,0 +1,12 @@
+/*var msg = prompt("What's on your mind");
+			var nmsg = prompt("What's NOT on your mind");
+			alert(msg);
+			alert(nmsg);
+	*/		
+var x = prompt("Please select a number between 0 and 9");
+ if (x==9){
+	 alert("Your number is " + x);
+ }
+ else {
+	 alert("Your number is NOT 9");
+	 }
